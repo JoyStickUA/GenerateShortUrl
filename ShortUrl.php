a9596a9fac48906b3bffbfa54bb57601560761c6
JoyStickUA <ntfake@yandex.ru>
2016-06-27 01:46:23 +0300

    CreateLink

--- ShortUrl.php
+++ ShortUrl.php
@@ -178,7 +178,114 @@ class ShortUrl
         $stmt->execute($params);
     }
 
+//    ----------------------------------------------------------------------------------------------------------------------
+    public function createPrivateShortCode($url, $private_url, $time)
+    {
+        if (empty($url)) {
+            throw new \Exception("Не получен адрес URL.");
+        }
 
+        if (empty($time)) {
+            $time = 0;
+        }
+
+        if (empty($private_url)) {
+            throw new \Exception("Не получен короткий адрес URL.");
+        }
+
+        if ($this->validateUrlFormat($url) == false) {
+            throw new \Exception(
+                "Адрес URL имеет неправильный формат.");
+        }
+
+        if ($this->validateLifeFormat($time) == false) {
+            throw new \Exception(
+                "Время жизни URL имеет неправильный формат.");
+        }
+
+        if (self::$checkUrlExists) {
+            if (!$this->verifyUrlExists($url)) {
+                throw new \Exception(
+                    "Адрес URL не существует.");
+            }
+        }
+
+        $count = $this->compareUrl($private_url);
+        if($count>0){
+            throw new \Exception(
+                "Адрес URL существует.");
+        }
+        $shortCode = $this->privateUrlExistsInDb($url, $private_url);
+        if ($time != 0 || $shortCode == false) {
+            $shortCode = $this->createPrivateShortUrl($url, $private_url, $time);
+        }
+        return $shortCode;
+    }
+
+    protected function compareUrl($private_url)
+    {
+        $query = "SELECT COUNT(*) FROM " . self::$table .
+            " WHERE short_code = :short_code " .
+            " OR private_url = :private_url";
+        $stmt = $this->pdo->prepare($query);
+        $params = array(
+            "short_code" => $private_url,
+            "private_url" => $private_url
+        );
+        $stmt->execute($params);
+
+        $result = $stmt->fetch();
+        return $result["COUNT(*)"];
+    }
+
+    protected function privateUrlExistsInDb($url, $private_url)
+    {
+        $query = "SELECT private_url, date_created, life_url FROM " . self::$table .
+            " WHERE long_url = :long_url " .
+            " AND private_url = :private_url LIMIT 1";
+        $stmt = $this->pdo->prepare($query);
+        $params = array(
+            "long_url" => $url,
+            "private_url" => $private_url
+        );
+        $stmt->execute($params);
+
+        $result = $stmt->fetch();
+        return !(empty($result['private_url']) || $result['short_created'] != $result['life_url']) ? false : $result["private_url"];
+    }
+
+    protected function createPrivateShortUrl($url, $private_url, $time)
+    {
+        $id = $this->insertUrlInDb($url);
+        $shortCode = $private_url;
+        $this->insertPrivateShortCodeInDb($id, $shortCode);
+        $this->LifeUrl($id, $time);
+        return $shortCode;
+    }
+
+    protected function insertPrivateShortCodeInDb($id, $private_url)
+    {
+        if ($id == null || $private_url == null) {
+            throw new \Exception("Параметры ввода неправильные.");
+        }
+        $query = "UPDATE " . self::$table .
+            " SET private_url = :private_url WHERE id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $params = array(
+            "private_url" => $private_url,
+            "id" => $id
+        );
+        $stmt->execute($params);
+
+        if ($stmt->rowCount() < 1) {
+            throw new \Exception(
+                "Строка не обновляется коротким кодом.");
+        }
+
+        return true;
+    }
+
+//    ----------------------------------------------------------------------------------------------------------------------
     public function shortCodeToUrl($code, $increment = true)
     {
         if (empty($code)) {
@@ -211,10 +318,12 @@ class ShortUrl
     protected function validateLifeShortCode($code)
     {
         $query = "SELECT date_created, life_url FROM " . self::$table .
-            " WHERE short_code = :short_code LIMIT 1";
+            " WHERE short_code = :short_code " .
+            "OR private_url = :private_url LIMIT 1";
         $stmt = $this->pdo->prepare($query);
         $params = array(
-            "short_code" => $code
+            "short_code" => $code,
+            "private_url" => $code
         );
         $stmt->execute($params);
         $result = $stmt->fetch();
@@ -235,10 +344,12 @@ class ShortUrl
     protected function getUrlFromDb($code)
     {
         $query = "SELECT id, long_url FROM " . self::$table .
-            " WHERE BINARY short_code = :short_code LIMIT 1";
+            " WHERE BINARY short_code = :short_code " .
+            "OR private_url = :private_url LIMIT 1";
         $stmt = $this->pdo->prepare($query);
         $params = array(
-            "short_code" => $code
+            "short_code" => $code,
+            "private_url" => $code
         );
         $stmt->execute($params);
 
--- index.php
+++ index.php
@@ -26,20 +26,20 @@ include 'header.php';
     <div class="hide" id="private">
         <form action="controller.php?" method="get" class="clearfix">
             <input type="hidden" name="action" placeholder="Time url" value="private">
-            <div class="form-group col-lg-4 col-md-5 col-sm-4 col-xs-12">
+            <div class="form-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
                 <input type="text" name="longUrl" placeholder="Paste a link to shorten it" class="form-control">
             </div>
-            <div class="form-group col-lg-3 col-md-4 col-sm-4 col-xs-12">
+            <div class="form-group col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                <input type="text" name="nameUrl" placeholder="Your Link" class="form-control">
+            </div>
+            <div class="form-group col-lg-2 col-md-2 col-sm-4 col-xs-12">
                 <input type="text" name="timeUrl" placeholder="Time url" class="form-control">
             </div>
-            <div class="form-group col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <input type="text" name="timeUrl" placeholder="Time url" class="form-control">
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
-                <button type="submit" class="btn btn-lg btn-primary reduce">Shorten</button>
+            <div class="col-lg-3 col-md-3 col-md-offset-0 col-sm-4 col-sm-offset-4 col-xs-12">
+                <button type="submit" class="btn btn-lg btn-primary reduce create">Shorten</button>
             </div>
         </form>
-        <div class="col-xs-12 col-lg-offset-1 col-lg-10">
+        <div class="col-xs-12 col-lg-10">
             <div class="btn btn-lg btn-success create_link" data-id="random" data-tab-id="private">Generate a link</div>
         </div>
     </div>
